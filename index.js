023a5b4ab0ece0ad25cafbec2383f798c74eb4db
Rahatul07 <rifarhad08@gmail.com>
2023-06-11 09:49:19 +0600

    improved

--- index.js
+++ index.js
@@ -71,19 +71,6 @@ async function run() {
       res.send({ token });
     });
 
-    // Warning: use verifyJWT before using verifyAdmin
-    // const verifyAdmin = async (req, res, next) => {
-    //   const email = req.decoded.email;
-    //   const query = { email: email };
-    //   const user = await usersCollection.findOne(query);
-    //   if (user?.role !== "admin") {
-    //     return res
-    //       .status(403)
-    //       .send({ error: true, message: "forbidden message" });
-    //   }
-    //   next();
-    // };
-    // add and update user
     app.put("/users", async (req, res) => {
       const user = re.body;
       const email = req.body.email;
@@ -158,14 +145,6 @@ async function run() {
       res.send(result);
     });
 
-    // users related apis
-    // app.get("/users/:email", async (req, res) => {
-    //   const email = req.params.email;
-    //   const filter = { email: email };
-    //   const result = await usersCollection.findOne(filter);
-    //   res.send(result);
-    // });
-
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log(user);
@@ -177,105 +156,13 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
-    // check admin
-    // app.get("/users/admin/:email", verifyJWT, async (req, res) => {
-    //   const email = req.params.email;
 
-    //   if (req.decoded.email !== email) {
-    //     res.send({ admin: false });
-    //   }
-
-    //   const query = { email: email };
-    //   const user = await usersCollection.findOne(query);
-    //   const result = { admin: user?.role === "admin" };
-    //   res.send(result);
-    // });
-
-    // app.patch("/users/admin/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   console.log(id);
-    //   const filter = { _id: new ObjectId(id) };
-    //   const updateDoc = {
-    //     $set: {
-    //       role: "admin",
-    //     },
-    //   };
-
-    //   const result = await usersCollection.updateOne(filter, updateDoc);
-    //   res.send(result);
-    // });
-    // Classes related api
-    // app.get("/classes", async (req, res) => {
-    //   const result = await classesCollection
-
-    //     .aggregate([
-    //       {
-    //         $addFields: {
-    //           totalStudents: { $toInt: "$totalStudents" },
-    //         },
-    //       },
-    //       {
-    //         $sort: { totalStudents: -1 },
-    //       },
-    //     ])
-    //     .toArray();
-    //   res.send(result);
-    // });
-
-    // app.patch("/classes/:id/select", async (req, res) => {
-    //   const { id } = req.params;
-    //   const { action } = req.body;
-
-    //   const course = await classesCollection.findOne({ _id: ObjectId(id) });
-
-    //   if (!course) {
-    //     return res.status(404).send({ message: "Course not found" });
-    //   }
-
-    //   if (action === "select") {
-    //     if (course.availableSeats > 0) {
-    //       await classesCollection.updateOne(
-    //         { _id: ObjectId(id) },
-    //         { $inc: { totalStudents: 1, availableSeats: -1 } }
-    //       );
-    //       res.send({ message: "Course selected successfully" });
-    //     } else {
-    //       res.status(400).send({ message: "Course is sold out" });
-    //     }
-    //   } else if (action === "deselect") {
-    //     if (course.totalStudents > 0) {
-    //       await classesCollection.updateOne(
-    //         { _id: ObjectId(id) },
-    //         { $inc: { totalStudents: -1, availableSeats: 1 } }
-    //       );
-    //       res.send({ message: "Course deselected successfully" });
-    //     } else {
-    //       res
-    //         .status(400)
-    //         .send({ message: "No students enrolled in the course" });
-    //     }
-    //   } else {
-    //     res.status(400).send({ message: "Invalid action" });
-    //   }
-    // });
-
-    // app.post("/classes", async (req, res) => {
-    //   const item = req.body;
-    //   const result = await classesCollection.insertOne(item);
-    //   res.send(result);
-    // });
     app.delete("/classes/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await classesCollection.deleteOne(query);
       res.send(result);
     });
-
-    // Instructors related api
-    // app.get("/instructors", async (req, res) => {
-    //   const result = await instructorsCollection.find().toArray();
-    //   res.send(result);
-    // });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -295,3 +182,116 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`language school is running on port: ${port}`);
 });
+
+// users related apis
+// app.get("/users/:email", async (req, res) => {
+//   const email = req.params.email;
+//   const filter = { email: email };
+//   const result = await usersCollection.findOne(filter);
+//   res.send(result);
+// });
+// Warning: use verifyJWT before using verifyAdmin
+// const verifyAdmin = async (req, res, next) => {
+//   const email = req.decoded.email;
+//   const query = { email: email };
+//   const user = await usersCollection.findOne(query);
+//   if (user?.role !== "admin") {
+//     return res
+//       .status(403)
+//       .send({ error: true, message: "forbidden message" });
+//   }
+//   next();
+// };
+// add and update user
+// check admin
+// app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+//   const email = req.params.email;
+
+//   if (req.decoded.email !== email) {
+//     res.send({ admin: false });
+//   }
+
+//   const query = { email: email };
+//   const user = await usersCollection.findOne(query);
+//   const result = { admin: user?.role === "admin" };
+//   res.send(result);
+// });
+
+// app.patch("/users/admin/:id", async (req, res) => {
+//   const id = req.params.id;
+//   console.log(id);
+//   const filter = { _id: new ObjectId(id) };
+//   const updateDoc = {
+//     $set: {
+//       role: "admin",
+//     },
+//   };
+
+//   const result = await usersCollection.updateOne(filter, updateDoc);
+//   res.send(result);
+// });
+// Classes related api
+// app.get("/classes", async (req, res) => {
+//   const result = await classesCollection
+
+//     .aggregate([
+//       {
+//         $addFields: {
+//           totalStudents: { $toInt: "$totalStudents" },
+//         },
+//       },
+//       {
+//         $sort: { totalStudents: -1 },
+//       },
+//     ])
+//     .toArray();
+//   res.send(result);
+// });
+
+// app.patch("/classes/:id/select", async (req, res) => {
+//   const { id } = req.params;
+//   const { action } = req.body;
+
+//   const course = await classesCollection.findOne({ _id: ObjectId(id) });
+
+//   if (!course) {
+//     return res.status(404).send({ message: "Course not found" });
+//   }
+
+//   if (action === "select") {
+//     if (course.availableSeats > 0) {
+//       await classesCollection.updateOne(
+//         { _id: ObjectId(id) },
+//         { $inc: { totalStudents: 1, availableSeats: -1 } }
+//       );
+//       res.send({ message: "Course selected successfully" });
+//     } else {
+//       res.status(400).send({ message: "Course is sold out" });
+//     }
+//   } else if (action === "deselect") {
+//     if (course.totalStudents > 0) {
+//       await classesCollection.updateOne(
+//         { _id: ObjectId(id) },
+//         { $inc: { totalStudents: -1, availableSeats: 1 } }
+//       );
+//       res.send({ message: "Course deselected successfully" });
+//     } else {
+//       res
+//         .status(400)
+//         .send({ message: "No students enrolled in the course" });
+//     }
+//   } else {
+//     res.status(400).send({ message: "Invalid action" });
+//   }
+// });
+
+// app.post("/classes", async (req, res) => {
+//   const item = req.body;
+//   const result = await classesCollection.insertOne(item);
+//   res.send(result);
+// });
+// Instructors related api
+// app.get("/instructors", async (req, res) => {
+//   const result = await instructorsCollection.find().toArray();
+//   res.send(result);
+// });
